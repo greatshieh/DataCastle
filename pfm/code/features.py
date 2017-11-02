91569c442979a51d8a094c130d86a0ab9cc15265
ivan.xiewei@gmail.com <ivan.xiewei@gmail.com>
2017-11-02 20:35:45 +0800

    得分： 0.90***7

--- pfm/code/features.py
+++ pfm/code/features.py
@@ -6,7 +6,10 @@ import pandas as pd
 import pprint
 
 from sklearn.ensemble import RandomForestClassifier
-# from sklearn.feature_selection import chi2
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import GenericUnivariateSelect
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -50,16 +53,47 @@ numeric_var = [
     if x != target_var and x not in character_var
 ]
 # 将数值型变量标准化
-scaler = MinMaxScaler()
-pattern = scaler.fit(df_train[numeric_var])
-df_train[numeric_var] = scaler.transform(df_train[numeric_var])
-df_test[numeric_var] = scaler.transform(df_test[numeric_var])
+#scaler = MinMaxScaler()
+#pattern = scaler.fit(df_train[numeric_var])
+#df_train[numeric_var] = scaler.transform(df_train[numeric_var])
+#df_test[numeric_var] = scaler.transform(df_test[numeric_var])
 
 df_train = pd.get_dummies(df_train)
 df_test = pd.get_dummies(df_test)
 
 predictor = [x for x in df_train.columns if x != target_var]
 
+'''
+def select_kbest_clf(data_frame, target, k=2):
+    """
+    Selecting K-Best features for classification
+    :param data_frame: A pandas dataFrame with the training data
+    :param target: target variable name in DataFrame
+    :param k: desired number of features from the data
+    :returns feature_scores: scores for each feature in the data as 
+    pandas DataFrame
+    """
+    feat_selector = GenericUnivariateSelect(f_classif, mode='fdr')
+    _ = feat_selector.fit(data_frame.drop(target, axis=1), data_frame[target])
+
+    feat_scores = pd.DataFrame()
+    feat_scores["F Score"] = feat_selector.scores_
+    feat_scores["P Value"] = feat_selector.pvalues_
+    feat_scores["Support"] = feat_selector.get_support()
+    feat_scores["Attribute"] = data_frame.drop(target, axis=1).columns
+
+    return feat_scores
+
+
+kbest_feat = select_kbest_clf(df_train, target_var, k=len(predictor))
+kbest_feat = kbest_feat.sort_values(
+    ["F Score", "P Value"], ascending=[False, False])
+pprint.pprint(kbest_feat)
+
+predictor = kbest_feat[kbest_feat['Support'] == True]['Attribute'].tolist()
+
+pprint.pprint(predictor)
+'''
 validation_size = 0.3
 seed = 7
 scoring = 'accuracy'
@@ -90,6 +124,10 @@ print("优化模型交叉验证分数: %f" % (grid_search.best_score_))
 pred_result = grid_search.best_estimator_.predict(X_test)
 pred_score = accuracy_score(y_test, pred_result)
 print('优化模型测试集分数： %.4f' % pred_score)
+cv_results = cross_val_score(
+    grid_search.best_estimator_, X_train, y_train, cv=kfold, scoring=scoring)
+msg = "优化模型交叉验证分数: %f (%f)" % (cv_results.mean(), cv_results.std())
+print(msg)
 print('最优化模型参数: ')
 pprint.pprint(grid_search.best_params_)
 sub_result = grid_search.best_estimator_.predict(df_test[predictor])
